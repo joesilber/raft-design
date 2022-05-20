corrected scipy rotation.from_euler call to intrinsic axes. however it's still not working right. suspicious of scipy

generate_DESI2_raft_layout.py
@@ -82,7 +82,8 @@ ax = fig.add_subplot(projection='3d', proj_type='ortho')
 outlines = []
 for row in t:
     basic = np.transpose([basic_raft_x, basic_raft_y, basic_raft_z])
-    r = Rotation.from_euler('zyz', [row['precession'], row['nutation'], row['spin']], degrees=True)
+    r = Rotation.from_euler('ZYZ',(row['precession'], row['nutation'], row['spin']), degrees=True)
+    print(r.as_euler('ZYZ', degrees=True))
     rotated = r.apply(basic)
     translated = rotated + [row['x'], row['y'], row['z']]
     f = np.transpose(translated)