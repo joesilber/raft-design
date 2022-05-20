8ef62377f6f90e959f26c8b359cadc8f0d78a7b7
joesilber <josephsilber@gmail.com>
2022-05-19 23:20:06 -0700

    chief ray deviation added

--- generate_DESI2_raft_layout.py
+++ generate_DESI2_raft_layout.py
@@ -10,9 +10,11 @@ import matplotlib.pyplot as plt
 # Z (mm) ... distance from origin CS5 parallel to optical axis
 # S (mm) ... integrated distance along surface from optical axis
 # N (deg) ... nutation angle (angle of positioner or fiducial local central axis)
+# CRD (deg) ... chief ray deviation
 Z = Polynomial([-2.33702E-05, 6.63924E-06, -1.00884E-04, 1.24578E-08, -4.82781E-10, 1.61621E-12, -5.23944E-15, 2.91680E-17, -7.75243E-20, 6.74215E-23])
 S = Polynomial([9.95083E-06, 9.99997E-01, 1.79466E-07, 1.76983E-09, 7.24320E-11, -5.74381E-13, 3.28356E-15, -1.10626E-17, 1.89154E-20, -1.25367E-23])
 N = Polynomial([1.79952E-03, 8.86563E-03, -4.89332E-07, -2.43550E-08, 9.04557E-10, -8.12081E-12, 3.97099E-14, -1.07267E-16, 1.52602E-19, -8.84928E-23])
+CRD = Polynomial([0, 3.4019e-3, -2.8068e-5, 4.4307e-7, -2.4009e-9, 5.1158e-12, -3.9825e-15])
 sphR = 4978  # mm, approx spherical radius of curvature
 
 # taken from desimeter/xy2qs.py on 2022-05-19
@@ -45,8 +47,9 @@ basic_raft_y += basic_raft_y
 basic_raft_z += [-L]*len(basic_raft_z)
 
 # nominal spacing between triangles
-spacing_rear = g + gcrn + 2*h2
+spacing_rear = g + 2*h2
 spacing_front = spacing_rear * sphR / (sphR - L)
+crd_margin = lambda radius: sphR * math.radians(CRD(radius + spacing_front) - CRD(radius))
 
 # table structure for raft positions and orientations
 t = Table(names=['x', 'y',  'z', 'radius', 'S', 'precession', 'nutation', 'spin'])
@@ -69,8 +72,9 @@ seed0 = {'x': 68.5, 'y': 56.0, 'spin': 180.0}
 fill_cols(seed0)
 t.add_row(seed0)
 
-seed1 = {'x': seed0['x'] + spacing_front*math.cos(math.radians(-30)),
-         'y': seed0['y'] + spacing_front*math.sin(math.radians(-30)),
+shift = spacing_front + crd_margin(seed0['radius'])
+seed1 = {'x': seed0['x'] + shift*math.cos(math.radians(-30)),
+         'y': seed0['y'] + shift*math.sin(math.radians(-30)),
          'spin': 0}
 fill_cols(seed1)
 t.add_row(seed1)
