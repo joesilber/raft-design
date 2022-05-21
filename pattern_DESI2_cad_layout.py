fe730f2289b006c944bb4a7718ddd58cbe24e7b5
joesilber <josephsilber@gmail.com>
2022-05-20 17:12:25 -0700

    minor bug fixes

--- pattern_DESI2_cad_layout.py
+++ pattern_DESI2_cad_layout.py
@@ -1,7 +1,8 @@
 # Joe Silber - 2022 - jhsilber@lbl.gov
 # must be run within the FreeCAD gui, due to usage of ImportGui module for making the STEP file at the end
 
-FREECADPATH = "C:/Program Files/FreeCAD0.19/bin" # path to your FreeCAD.so or FreeCAD.dll file
+
+FREECADPATH = 'C:/Program Files/FreeCAD 0.19/bin' # path to your FreeCAD.so or FreeCAD.dll file
 import sys
 sys.path.append(FREECADPATH)
 import math
@@ -16,11 +17,11 @@ script_title = "DESI2 Raft Patterning Script"
 doc_name = "PatternDoc"
 App.newDocument(doc_name)
 AD = App.ActiveDocument		# just to make things more readable
-starttime = time.clock()
+starttime = time.time()
 print("\nBEGIN " + script_title + "...") # print the script name
 
 # Paths to source model
-homepath = '~' #"C:/Users/jhsilber/Documents/PDMWorks/"
+homepath = 'C:/Users/joe/' #"C:/Users/jhsilber/Documents/PDMWorks/"
 source_model = "MM Raft Assembly - simplified - 2022-05-19.STEP"
 model_path = os.path.join(homepath, source_model)
 base_name = "EnvelopesArray"
@@ -28,14 +29,14 @@ base_name = "EnvelopesArray"
 # Read in the source geometry
 source_name  = "proto"
 source       = AD.addObject("Part::Feature", source_name)
-source.Shape = Part.read(homepath + source_model)
+source.Shape = Part.read(model_path)
 
 # Read in the raft positions
 pattern_name = '20220520T1631_desi2_layout_21rafts_1512robots.csv'
 pattern_path = os.path.join(homepath, pattern_name)
 tbl = Table.read(pattern_path)
 
-steptime = time.clock()
+steptime = time.time()
 print(f'... {len(tbl)} raft positions read')
 lasttime = steptime
 
@@ -60,9 +61,12 @@ for i in range(num_to_process):
     q3 = math.cos(s/2)*math.cos(n/2)*math.sin(p/2) + math.cos(n/2)*math.cos(p/2)*math.sin(s/2)
     q4 = math.cos(s/2)*math.cos(n/2)*math.cos(p/2) - math.sin(s/2)*math.cos(n/2)*math.sin(p/2)
     rafts[-1].Placement.Rotation = Base.Rotation(q1, q2, q3, q4)
-    rafts[-1].Placement.Base = Base.Vector(tbl['x'], tbl['y'], tbl['z'])
+    x = float(tbl['x'][i])
+    y = float(tbl['y'][i])
+    z = float(tbl['z'][i])
+    rafts[-1].Placement.Base = Base.Vector(x, y, z)
 
-steptime = time.clock()
+steptime = time.time()
 print(f'... {len(rafts)} rafts patterned in {(steptime-lasttime)/60:.2f} min')
 lasttime = steptime
 
@@ -74,11 +78,11 @@ ImportGui.export(rafts, export_path) # requires GUI, does the export of geometry
 App.getDocument("PatternDoc").removeObject("proto") # deletes the proto raft, just to give user warm-fuzzies about what was exported
 Gui.SendMsgToActiveView("ViewFit") # requires GUI, gives user warm-fuzzies
 Gui.activeDocument().activeView().viewAxometric() # requires GUI, gives user warm-fuzzies
-steptime = time.clock()
+steptime = time.time()
 print(f'... exported array in {(steptime-lasttime)/60:.2f} min')
 lasttime = steptime
 
-endtime = time.clock()
+endtime = time.time()
 runtime = endtime-starttime
 print('... DONE')
 print(f'Total runtime = {runtime/60:.2f} min\n')
